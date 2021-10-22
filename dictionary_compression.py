a0eb4335e90a60378ca6c9a2b306595d884ea3b9
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2021-10-22 17:57:30 -0400

    .

--- dictionary_compression.py
+++ dictionary_compression.py
@@ -4,6 +4,9 @@ from nltk import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
+from naive_indexer import select_doc_id
+
+
 def select_tokenized_document(string_document):
     return RegexpTokenizer(r'\w+').tokenize(string_document)
 
@@ -17,7 +20,9 @@ def select_nonpositional_posting_token_list():
     for file_name in os.listdir("reuters21578")[:5]:
         file_content = select_file_content(file_name)
         tokenized_content = list(set(select_tokenized_document(file_content)))
-        token_list += tokenized_content
+        doc_id = select_doc_id(file_content)
+        token_list += list(map(lambda token: (token, doc_id), tokenized_content))
+        token_list = list(filter(lambda token_docId: token_docId != "UNKNOWN", token_list))
     return token_list
 
 
@@ -26,7 +31,9 @@ def select_positional_posting_token_list():
     for file_name in os.listdir("reuters21578")[:5]:
         file_content = select_file_content(file_name)
         tokenized_content = select_tokenized_document(file_content)
-        token_list += tokenized_content
+        doc_id = select_doc_id(file_content)
+        token_list += list(map(lambda token: (token, doc_id), tokenized_content))
+        token_list = list(filter(lambda token_docId: token_docId != "UNKNOWN", token_list))
     return token_list
 
 
@@ -40,22 +47,37 @@ def select_token_set():
     return token_set
 
 
-def select_token_list_without_numbers(token_list):
-    return list(filter(lambda token: not token.isdigit(), token_list))
+def select_token_list_without_numbers(token_list, is_posting):
+    if is_posting:
+        return list(filter(lambda token_docId: not (token_docId[0].isdigit()), token_list))
+    else:
+        return list(filter(lambda token: not token.isdigit(), token_list))
 
 
-def select_lower_case_token_list(token_list):
-    return set(map(lambda token: token.lower(), token_list))
+def select_lower_case_token_list(token_list, is_posting, is_positional):
+    if is_posting:
+        lower_case_token_list = list(
+            map(lambda token_docId: (token_docId[0].lower(), token_docId[1]), token_list))
+        return set(lower_case_token_list) if not is_positional else lower_case_token_list
+    else:
+        return set(map(lambda token: token.lower(), token_list))
 
 
-def select_token_list_without_stop_words(token_list):
+def select_token_list_without_stop_words(token_list, is_posting):
     english_stopwords = stopwords.words("english")
-    return list(filter(lambda string: string not in english_stopwords, token_list))
+    if is_posting:
+        return list(filter(lambda token_docId: token_docId[0] not in english_stopwords, token_list))
+    else:
+        return list(filter(lambda string: string not in english_stopwords, token_list))
 
 
-def select_stemmed_token_list(token_list):
+def select_stemmed_token_list(token_list, is_posting, is_positional):
     stemmer = PorterStemmer()
-    return list(map(lambda string: stemmer.stem(string), token_list))
+    if is_posting:
+        stemmed_token_list = map(lambda token_docId: stemmer.stem(token_docId[0], token_docId[1]), token_list)
+        return list(set(stemmed_token_list)) if not is_positional else list(stemmed_token_list)
+    else:
+        return list(map(lambda string: stemmer.stem(string), token_list))
 
 
 def add_entry_global_index(global_index, token, doc_id):
@@ -65,61 +87,48 @@ def add_entry_global_index(global_index, token, doc_id):
         global_index[token] = [doc_id]
 
 
-def select_compressed_token_list(token_list):
-    token_list_without_numbers = select_token_list_without_numbers(token_list)
-    token_list_lower_case = select_lower_case_token_list(token_list_without_numbers)
-    token_list_without_stop_words = select_token_list_without_stop_words(token_list_lower_case)
-    return select_stemmed_token_list(token_list_without_stop_words)
-
-
-def select_compressed_document_posting_list(token_list, doc_id):
-    compressed_token_list = select_compressed_token_list(token_list)
-    posting_list = {}
-    for token in compressed_token_list:
-        add_entry_global_index(posting_list, token, doc_id)
-    return posting_list
-
-
 def output_variation(original_list, new_list):
     original_list_length = len(original_list)
     new_list_length = (len(new_list))
     print("Variation -" + str(100 - int((new_list_length / original_list_length) * 100)) + "%\n")
 
 
-def output_compression_stats(token_list, category):
+def output_compression_stats(token_list, category, is_posting, is_positional=False):
     print("\n -- " + category + " -- ")
     print("Unfiltered: " + str(len(token_list)))
 
-    token_list_without_numbers = select_token_list_without_numbers(token_list)
+    token_list_without_numbers = select_token_list_without_numbers(token_list, is_posting)
     print("No numbers: " + str(len(token_list_without_numbers)))
     output_variation(token_list, token_list_without_numbers)
 
-    token_list_lower_case = select_lower_case_token_list(token_list_without_numbers)
+    token_list_lower_case = select_lower_case_token_list(token_list_without_numbers, is_posting,
+                                                         is_positional)
     print("Case folding: " + str(len(token_list_lower_case)))
     output_variation(token_list_without_numbers, token_list_lower_case)
 
-    token_list_without_stop_words = select_token_list_without_stop_words(token_list_lower_case)
+    token_list_without_stop_words = select_token_list_without_stop_words(token_list_lower_case,
+                                                                         is_posting)
     print("Without stop words: " + str(len(token_list_without_stop_words)))
     output_variation(token_list_lower_case, token_list_without_stop_words)
 
-    token_list_stemmed = select_stemmed_token_list(token_list_without_stop_words)
+    token_list_stemmed = select_stemmed_token_list(token_list_without_stop_words, is_posting, is_positional)
     print("Stemming: " + str(len(token_list_stemmed)))
     output_variation(token_list_without_stop_words, token_list_stemmed)
 
 
 def output_distinct_term_stats():
     token_list = select_token_set()
-    output_compression_stats(token_list, "Distinct terms")
+    output_compression_stats(token_list, "Distinct terms", False)
 
 
 def output_non_positional_stats():
     token_list = select_nonpositional_posting_token_list()
-    output_compression_stats(token_list, "Non positional postings")
+    output_compression_stats(token_list, "Non positional postings", True)
 
 
 def output_positional_posting_stats():
     token_list = select_positional_posting_token_list()
-    output_compression_stats(token_list, "Positional postings")
+    output_compression_stats(token_list, "Positional postings", True, True)
 
 
 output_distinct_term_stats()
